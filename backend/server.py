4617ee77f740c3dd5a4cf3eea8323ce1e23874c8
Jean Flores Acid <jeanflores.acidlabs@latam.com>
2022-06-28 17:43:10 -0500

    Add comments

--- backend/server.py
+++ backend/server.py
@@ -29,70 +29,20 @@ def read_embargos(page):
         return e
     return jsonify({"rows": result})
 
-### Cambio por resultados
-@app.route("/read_products", defaults={"page": 1})
-@app.route("/read_products/page/<int:page>", methods=["GET"])
+@app.route("/read_resultados", defaults={"page": 1})
+@app.route("/read_resultados/page/<int:page>", methods=["GET"])
 def read_productos(page):
     try:
         start_at = page * ROWS_PER_REQUEST
         print(" GET ", request)
         print(start_at)
         print(page)
-        query = ("SELECT * FROM productos order by name asc limit %s, %s;", (start_at, ROWS_PER_REQUEST))
+        query = ("SELECT * FROM resultados order by encargado asc limit %s, %s;", (start_at, ROWS_PER_REQUEST))
         result = db.read(query)
         print(result)
     except Exception as e:
         return e
     return jsonify({"rows": result})
-
-@app.route("/add_embargo", methods=["POST"])
-def add_embargo():
-    if request.method == "POST":
-        print("POST")
-        try:
-            print(request)
-            query = '''
-                INSERT INTO embargos(
-                estado,
-                anotacion,
-                localidad,
-                complejidad,
-                detalle_complejidad,
-                encargado,
-                fecha_recepcion,
-                hora_recepcion,
-                job_id,
-                fecha_embargo,
-                id_oracle,
-                efectivo,
-                tipo_documento,
-                numero_documento,
-                tipo_cuenta,
-                numero_cuenta
-                ) VALUES(%s, %s, %s, %s,%s, %s, %s, %s,%s, %s, %s, %s,%s, %s, %s, %s,)
-            ''', (
-                    request.json['estado'], 
-                    request.json['anotacion'], 
-                    request.json['localidad'],
-                    request.json['complejidad'], 
-                    request.json['detalle_complejidad'], 
-                    request.json['encargado'],
-                    request.json['fecha_recepcion'], 
-                    request.json['hora_recepcion'], 
-                    request.json['job_id'],
-                    request.json['fecha_embargo'], 
-                    request.json['id_oracle'], 
-                    request.json['efectivo'],
-                    request.json['tipo_documento'], 
-                    request.json['numero_documento'], 
-                    request.json['tipo_cuenta'],
-                    request.json['numero_cuenta'],
-                )
-            db.insert(query)
-        except Exception as e:
-            print(e)
-            return e
-    return jsonify({ "add_embargo": request.json["numero_cuenta"]})
 
 if __name__ == "__main__":
     app.debug = True
